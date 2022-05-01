95cec83f73f7c2ccd1c3a578ab76dfd367be2ef5
RaduLupan <github@lupan.ca>
2022-05-01 12:35:00 -0400

    Set attributes on sqs queue as params

--- aws-sdk-examples/sqs-ex1.py
+++ aws-sdk-examples/sqs-ex1.py
@@ -3,9 +3,17 @@ import boto3
 # Get the service resource
 sqs = boto3.resource('sqs')
 
-# Create the queue. This returns an SQS.Queue instance
-queue = sqs.create_queue(QueueName='test', Attributes={'DelaySeconds': '5'})
+# Inputs
+print("Enter a name for the SQS queue")
+sqs_name=input()
 
-# You can now access identifiers and attributes
-print(queue.url)
-print(queue.attributes.get('DelaySeconds'))
+print("Enter delay in seconds")
+sqs_delay_seconds=input()
+
+
+# Create the queue. This returns an SQS.Queue instance
+queue = sqs.create_queue(QueueName=sqs_name, Attributes={'DelaySeconds': sqs_delay_seconds})
+
+# Outputs
+print("SQS URL: " +queue.url)
+print("SQS Delay Seconds: " +queue.attributes.get('DelaySeconds'))
