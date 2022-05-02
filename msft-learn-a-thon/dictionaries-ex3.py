429cebe59c7e9605fab805253577c3946d282afd
RaduLupan <github@lupan.ca>
2022-05-02 09:26:39 -0400

    Modified example for use in tags key-value pairs

--- msft-learn-a-thon/dictionaries-ex3.py
+++ msft-learn-a-thon/dictionaries-ex3.py
@@ -1,5 +1,10 @@
-class_list = dict() 
-data = input('Enter name & score separated by ":" ') 
-temp = data.split(':') 
-class_list[temp[0]] = int(temp[1]) 
-print (class_list)
+tags = dict() 
+user_input=""
+
+while user_input != "exit": 
+    user_input = input('Enter key & value separated by ":" ') 
+    key_value = user_input.split(':')
+    print(key_value) 
+    tags[key_value[0]]=key_value[1]
+    print(tags)    
+
