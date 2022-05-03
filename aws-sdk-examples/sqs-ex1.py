b52b4ba98d558405593cde7b2f6b18323883fa78
RaduLupan <github@lupan.ca>
2022-05-03 09:40:14 -0400

    Added vars for the config file, opened the file for read, read all lines, striped the new line and built the attributes dictionary

--- aws-sdk-examples/sqs-ex1.py
+++ aws-sdk-examples/sqs-ex1.py
@@ -7,6 +7,9 @@ sqs = boto3.resource('sqs')
 common_tags_dictionary = dict()
 sqs_attributes_dictionary = dict()
 
+config_file_name = "sqs-ex1.config"
+config_file_lines = []
+
 # Inputs
 sqs_name=input("Enter a name for the SQS queue: ")
 
@@ -24,6 +27,20 @@ while user_input != "exit":
         common_tags_dictionary[key_value[0]]=key_value[1]
         print(f"common_tags_dictionary: {common_tags_dictionary}")    
 
+
+# Read all lines from the config file.
+with open(config_file_name,'r') as f:
+    config_file_lines = f.readlines()
+
+# Split all lines read from the config file in key:value and populate the attributes dictionary. Make sure you strip the new line character.
+count = 0
+for line in config_file_lines:
+    count += 1
+    line=line.strip('\n')
+    key_value=line.split(":")
+    sqs_attributes_dictionary[key_value[0]]=key_value[1]
+
+print(sqs_attributes_dictionary)
 
 # Create the queue. This returns an SQS.Queue instance
 queue = sqs.create_queue(QueueName=sqs_name, Attributes=sqs_attributes_dictionary, tags=common_tags_dictionary)
