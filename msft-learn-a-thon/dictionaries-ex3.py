6dbacbb4c74347b74cf4e6bfb77f6f03336e8e8f
RaduLupan <github@lupan.ca>
2022-05-02 20:35:05 -0400

    Fixed index out of range error by introducing the if clause in the while loop.

--- msft-learn-a-thon/dictionaries-ex3.py
+++ msft-learn-a-thon/dictionaries-ex3.py
@@ -1,10 +1,16 @@
-tags = dict() 
-user_input=""
+tags_dictionary = dict() 
 
-while user_input != "exit": 
-    user_input = input('Enter key & value separated by ":" ') 
-    key_value = user_input.split(':')
-    print(key_value) 
-    tags[key_value[0]]=key_value[1]
-    print(tags)    
+user_input="1"
 
+while user_input != "exit":
+    user_input = input('Enter key & value separated by ":" ')
+    print(f"User input: {user_input}")
+    
+    # Need this test to avoid running the code below when user_input="exit" which results in 'list index out of range' error!
+    if user_input != "exit":
+        key_value = user_input.split(":")
+        print(f"key_value: {key_value}") 
+        
+        # Adds key:value pair to the tags dictionary.
+        tags_dictionary[key_value[0]]=key_value[1]
+        print(f"tags_dictionary: {tags_dictionary}")    
