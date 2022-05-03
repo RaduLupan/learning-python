65c255350488d27c3f02c8a74ed398e5026cca69
RaduLupan <github@lupan.ca>
2022-05-02 20:48:57 -0400

    Added building of dictonary tags from user input.

--- aws-sdk-examples/sqs-ex1.py
+++ aws-sdk-examples/sqs-ex1.py
@@ -3,14 +3,31 @@ import boto3
 # Get the service resource
 sqs = boto3.resource('sqs')
 
+# Local variables
+common_tags_dictionary = dict()
+
 # Inputs
 sqs_name=input("Enter a name for the SQS queue: ")
 
 sqs_delay_seconds=input("Enter delay in seconds: ")
 
+user_input=""
+while user_input != "exit":
+    user_input = input('Tags - Enter key & value separated by ":" ')
+    print(f"User input: {user_input}")
+    
+    # Need this test to avoid running the code below when user_input="exit" which results in 'list index out of range' error!
+    if user_input != "exit":
+        key_value = user_input.split(":")
+        print(f"key_value: {key_value}") 
+        
+        # Adds key:value pair to the tags dictionary.
+        common_tags_dictionary[key_value[0]]=key_value[1]
+        print(f"common_tags_dictionary: {common_tags_dictionary}")    
+
 
 # Create the queue. This returns an SQS.Queue instance
-queue = sqs.create_queue(QueueName=sqs_name, Attributes={'DelaySeconds': sqs_delay_seconds}, tags={'environment': 'dev'})
+queue = sqs.create_queue(QueueName=sqs_name, Attributes={'DelaySeconds': sqs_delay_seconds}, tags=common_tags_dictionary)
 
 # Outputs
 print("SQS URL: " +queue.url)
