e331049110841cf2a54526954802e158fbe1e7e6
RaduLupan <github@lupan.ca>
2022-05-04 08:50:41 -0400

    Made the while loop run for ever and break when user input is exit.

--- msft-learn-a-thon/dictionaries-ex3.py
+++ msft-learn-a-thon/dictionaries-ex3.py
@@ -2,15 +2,16 @@ tags_dictionary = dict()
 
 user_input="1"
 
-while user_input != "exit":
-    user_input = input('Enter key & value separated by ":" ')
+while True:   
+    user_input = input('Enter key & value separated by ":" or "exit" to finish \n')
     print(f"User input: {user_input}")
     
-    # Need this test to avoid running the code below when user_input="exit" which results in 'list index out of range' error!
-    if user_input != "exit":
-        key_value = user_input.split(":")
-        print(f"key_value: {key_value}") 
+    if user_input == "exit":
+        break
+
+    key_value = user_input.split(":")
+    print(f"key_value: {key_value}") 
         
-        # Adds key:value pair to the tags dictionary.
-        tags_dictionary[key_value[0]]=key_value[1]
-        print(f"tags_dictionary: {tags_dictionary}")    
+    # Adds key:value pair to the tags dictionary.
+    tags_dictionary[key_value[0]]=key_value[1]
+    print(f"tags_dictionary: {tags_dictionary}")    
