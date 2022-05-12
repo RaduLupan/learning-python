290222e1b193d77afb02081b08b606691aae4d8b
RaduLupan <github@lupan.ca>
2022-05-12 08:36:57 -0400

    Modified scope to only validate public status for 1 bucket whose name is read as user input, finalized script.

--- aws-sdk-examples/s3-ex1.py
+++ aws-sdk-examples/s3-ex1.py
@@ -1,34 +1,25 @@
-#-------------------------------------------------------------------------------------------
-# This exemple lists all the S3 buckets that have public access turned ON in an AWS account.
-# #-----------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------
+# This exemple evaluates if a given S3 bucket is public due to public ACL or bucket policy.
+# -----------------------------------------------------------------------------------------
 
 # References:
 #------------
 
 # 1: https://www.slsmk.com/programmatically-set-public-block-on-aws-s3-buckets/
 # 2: https://stackoverflow.com/questions/59002558/boto-find-if-bucket-is-public-or-private
+# 3: https://docs.aws.amazon.com/AmazonS3/latest/userguide/access-control-block-public-access.html
 
 import boto3
 import botocore.session
 from botocore.exceptions import ClientError
 
-# Get the service resource
-# s3 = boto3.resource('s3')
-
-# Print out bucket names
-#for bucket in s3.buckets.all():
-#    print(bucket.name)
-
-#buckets = list(s3.buckets.all())
-# print(buckets)
-
-bucket_name='test-bucket'
-s3 = boto3.client('s3')
-
-
+# Local variables.
 bucket_properties=dict()
 public_acl=False
+public_policy=False
 
+bucket_name = input ("Enter S3 bucket name:")
+s3 = boto3.client('s3')
 
 # Get public access block.
 try:
@@ -39,7 +30,7 @@ try:
 
 except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == 'NoSuchPublicAccessBlockConfiguration':
-        print(f"Bucket {bucket_name} has never been configured for Public Access Block and all 4 swithces are False by default.")
+        print(f"Bucket {bucket_name} has never been configured for Public Access Block and all 4 flags are False by default.\n")
         bucket_properties['BlockPublicAcls']=False
         bucket_properties['IgnorePublicAcls']=False
         bucket_properties['BlockPublicPolicy']=False
@@ -48,13 +39,22 @@ except botocore.exceptions.ClientError as e:
         print("Unexpected error: %s" % (e.response))
 
 
+# IMPORTANT - Note from AWS Documentation
+# --------------------------------------------------------------------------------------------------------------------------------------
+# Calls to GET Bucket acl and GET Object acl always return the effective permissions in place for the specified bucket or object. 
+# For example, suppose that a bucket has an ACL that grants public access, but the bucket also has the IgnorePublicAcls setting enabled.
+# In this case, GET Bucket acl returns an ACL that reflects the access permissions that Amazon S3 is enforcing, rather than the actual 
+# ACL that is associated with the bucket.
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
 # Get bucket ACL.
 try:
     bucket_acl = s3.get_bucket_acl(Bucket=bucket_name)
+    
     bucket_properties['Owner']=bucket_acl['Owner']
     bucket_properties['Grants']=bucket_acl['Grants']
 
-    print(f"Bucket Grants: {bucket_acl['Grants']}")
+    # print(f"Bucket Grants: {bucket_acl['Grants']}\n")
 
     # Amazon S3 considers a bucket or object ACL public if it grants any permissions to members of the predefined AllUsers or AuthenticatedUsers groups.
     # See The meaning of "Public" section here: https://docs.aws.amazon.com/AmazonS3/latest/userguide/access-control-block-public-access.html 
@@ -69,8 +69,13 @@ except botocore.exceptions.ClientError as e:
 # Get bucket policy status.
 try:
     bucket_policy_status = s3.get_bucket_policy_status(Bucket=bucket_name)
-    print(f"Bucket policy status is: {bucket_policy_status}")
+    
     bucket_properties['PolicyStatus']=bucket_policy_status['PolicyStatus']
+    
+    # print(f"Bucket policy status is: {bucket_policy_status}\n")
+
+    public_policy = bucket_policy_status['PolicyStatus']['IsPublic']
+
 except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == 'NoSuchBucketPolicy':
         print(f"Bucket {bucket_name} does not have a bucket policy.")
@@ -79,10 +84,15 @@ except botocore.exceptions.ClientError as e:
         print("Unexpected error: %s" % (e.response))
 
 
-
 bucket_properties['PublicACL']=public_acl
+bucket_properties['PublicPolicy']=public_policy
 
-print(f"Bucket {bucket_name} has the following properties: {bucket_properties}")
+if public_acl or public_policy:
+    print(f"Bucket {bucket_name} is public!\n")
+else:
+    print(f"Bucket {bucket_name} is not public.\n")
+
+print(f"Bucket {bucket_name} properties:\n {bucket_properties}\n")
 
 
 
