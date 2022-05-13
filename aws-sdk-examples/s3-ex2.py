1f4f186ded22672248cc08b54927f62932578bd6
RaduLupan <github@lupan.ca>
2022-05-13 08:47:35 -0400

    Renamed function, let it return the entire dictonary of bucket properties.

--- aws-sdk-examples/s3-ex2.py
+++ aws-sdk-examples/s3-ex2.py
@@ -1,17 +1,22 @@
 import boto3
+#---------------------------------------------------------------------------------------------------------
+# This exemple expands on s3-ex1.py and uses a Python function to evaluate if a given S3 bucket is public.
+# --------------------------------------------------------------------------------------------------------
+
 import botocore.session
 from botocore.exceptions import ClientError
 
-def is_bucket_public (bucket_name):
+bucket_properties = dict()
+
+def evaluate_s3_public_access (bucket_name):
     
     s3 = boto3.client('s3')
 
     public_acl=True
     public_policy = False
     bucket_properties = {'Name':bucket_name, 'Grants':'Some grants'}
-    if public_acl or public_policy:
-        return True
+    
+    return bucket_properties
 
-
-public_flag=is_bucket_public (bucket_name='test')
-print(public_flag)
+bucket_properties=evaluate_s3_public_access (bucket_name='test')
+print(bucket_properties)
