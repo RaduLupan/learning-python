c08b3f7d2766a6c24295e6a5380c84700a8cb214
RaduLupan <github@lupan.ca>
2022-05-18 08:05:41 -0400

    Added append_value function and call example

--- google-sheets-api/gsheets-ex1.py
+++ google-sheets-api/gsheets-ex1.py
@@ -30,6 +30,32 @@ def get_values(creds, spreadsheet_id, range_name):
         print(f"An error occurred: {error}")
         return error
 
+def append_values(creds, spreadsheet_id, range_name, value_input_option,
+                  _values):
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        values = [
+            [
+                # Cell values ...
+            ],
+            # Additional rows ...
+        ]
+        # [START_EXCLUDE silent]
+        values = _values
+        # [END_EXCLUDE]
+        body = {
+            'values': values
+        }
+        result = service.spreadsheets().values().append(
+            spreadsheetId=spreadsheet_id, range=range_name,
+            valueInputOption=value_input_option, body=body).execute()
+        print(f"{(result.get('updates').get('updatedCells'))} cells appended.")
+        return result
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
 
 def main():
     scopes = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
@@ -45,7 +71,16 @@ def main():
         scopes,
     )
 
+    # Get values from sample spreadsheet.
     result = get_values(creds, sample_spreadsheet_id, sample_range_name)
+    print(result)
+
+    # Append values to sample spreadsheet.
+    result = append_values(creds, sample_spreadsheet_id, "A1:C2", "USER_ENTERED",
+             [
+                 ['F', 'B'],
+                 ['C', 'D']
+            ])
     print(result)
 
 if __name__ == '__main__':
