8282f581cd11d94ac690b9bd9c97864e3487b82c
RaduLupan <github@lupan.ca>
2022-05-18 11:13:17 -0400

    Added comments

--- google-sheets-api/module1.py
+++ google-sheets-api/module1.py
@@ -1,5 +1,11 @@
+# References:
+#------------
+
+# 1:Python Tutorial if __name__ == '_main__' -> https://www.youtube.com/watch?v=sugvnHA7ElY
+
 def main():
     print(f"Printing from module 1 - __name__ = {__name__}")
 
+# This ensures the code runs only when Python runs this file directly (not via import).
 if __name__ == '__main__' :
     main()
