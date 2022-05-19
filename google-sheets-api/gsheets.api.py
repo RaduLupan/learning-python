988358c4537485cd1743945b6df961d759ef1d48
RaduLupan <github@lupan.ca>
2022-05-19 07:35:36 -0400

    Initial commit

---
+++ google-sheets-api/gsheets.api.py
@@ -0,0 +1,54 @@
+'''
+-------------------------------------------------------------
+This module contains functions to use with Google Sheets API.
+-------------------------------------------------------------
+'''
+
+import google.auth
+
+from __future__ import print_function
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from oauth2client.service_account import ServiceAccountCredentials
+
+def get_values(creds, spreadsheet_id, range_name):
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        result = service.spreadsheets().values().get(
+            spreadsheetId=spreadsheet_id, range=range_name).execute()
+        rows = result.get('values', [])
+        print(f"{len(rows)} rows retrieved")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+def append_values(creds, spreadsheet_id, range_name, value_input_option,
+                  _values):
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        values = [
+            [
+                # Cell values ...
+            ],
+            # Additional rows ...
+        ]
+        # [START_EXCLUDE silent]
+        values = _values
+        # [END_EXCLUDE]
+        body = {
+            'values': values
+        }
+        result = service.spreadsheets().values().append(
+            spreadsheetId=spreadsheet_id, range=range_name,
+            valueInputOption=value_input_option, body=body).execute()
+        print(f"{(result.get('updates').get('updatedCells'))} cells appended.")
+        return result
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
