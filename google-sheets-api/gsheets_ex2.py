1f0bb6cfa11673d721dbab5005fe67b6e90ae108
RaduLupan <github@lupan.ca>
2022-05-19 08:08:38 -0400

    Added function calls from gsheets_api module

--- google-sheets-api/gsheets_ex2.py
+++ google-sheets-api/gsheets_ex2.py
@@ -8,3 +8,34 @@ References:
 2: https://github.com/googleapis/google-api-python-client/tree/main/samples
 3: https://github.com/googleworkspace/python-samples
 '''
+
+import gsheets_api
+
+def main():
+    scopes = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+
+    service_account_file = 'credentials.json'
+
+    # The ID and range of a sample spreadsheet.
+    sample_spreadsheet_id = '1z36C1xvQwrvrxyLlYIHx5wTZFt_BpOYi-q6DF_2B39g'
+    sample_range_name = 'A1:A4'
+
+    creds = gsheets_api.ServiceAccountCredentials.from_json_keyfile_name(
+        service_account_file,
+        scopes,
+    )
+
+    # Get values from sample spreadsheet.
+    result = gsheets_api.get_values(creds, sample_spreadsheet_id, sample_range_name)
+    print(result)
+
+    # Append values to sample spreadsheet.
+    result = gsheets_api.append_values(creds, sample_spreadsheet_id, "A1:C2", "USER_ENTERED",
+             [
+                 ['F', 'B'],
+                 ['C', 'D']
+            ])
+    print(result)
+
+if __name__ == '__main__':
+    main()
