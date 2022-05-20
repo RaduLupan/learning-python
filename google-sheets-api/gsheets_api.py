7655470a01ac728ea55e7ece493eb8329166aaec
RaduLupan <github@lupan.ca>
2022-05-20 10:20:36 -0400

    Added create_sheet function

--- google-sheets-api/gsheets_api.py
+++ google-sheets-api/gsheets_api.py
@@ -8,20 +8,23 @@ from __future__ import print_function
 
 import google.auth
 
-from googleapiclient.discovery import build
+from googleapiclient.discovery import build    #  !!
 from googleapiclient.errors import HttpError
 
-from oauth2client.service_account import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials  # !!
 
 def get_values(creds, spreadsheet_id, range_name):
     try:
         service = build('sheets', 'v4', credentials=creds)
 
-        result = service.spreadsheets().values().get(
+        # Call the Sheets API.
+        sheet = service.spreadsheets()
+
+        result = sheet.values().get(
             spreadsheetId=spreadsheet_id, range=range_name).execute()
         rows = result.get('values', [])
         print(f"{len(rows)} rows retrieved")
-        return result
+        return rows
     except HttpError as error:
         print(f"An error occurred: {error}")
         return error
@@ -49,6 +52,18 @@ def append_values(creds, spreadsheet_id, range_name, value_input_option,
         print(f"{(result.get('updates').get('updatedCells'))} cells appended.")
         return result
 
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+def create_sheet(creds,title,spreadsheet_id):
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+        
+        batch_update_spreadsheet_request_body = {'requests':[{'addSheet': {'properties': {'title': title}}}]}
+        
+        request = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id, body=batch_update_spreadsheet_request_body)
+        response = request.execute()
     except HttpError as error:
         print(f"An error occurred: {error}")
         return error
