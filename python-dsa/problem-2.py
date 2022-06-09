5d83669704e7c2d2d4ce643b9cb977a5511af807
RaduLupan <github@lupan.ca>
2022-06-09 09:22:59 -0400

    Added generic binary_search function and function signatures for locate_start_position and locate_end_position.

--- python-dsa/problem-2.py
+++ python-dsa/problem-2.py
@@ -8,3 +8,48 @@ You must write an algorithm with O(log n) runtime complexity.
 Source:
 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 '''
+def locate_start_position(array, target):
+    pass
+
+def locate_end_position(array, target):
+    pass 
+
+def locate_start_end_position(array, target):
+
+    start = locate_start_position(array, target)
+    end = locate_end_position(array, target)
+
+    return [start, end]
+
+def binary_search(lo,hi,condition):
+    '''
+    Description: Generic algorithm for binary search. The condition argument is a function that determines whether the answer lies before, after or at a given position.
+    Use in conjunction with locate_card function.
+    '''
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        
+        result = condition(mid)
+
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid-1
+        elif result == 'right':
+            lo = mid+1
+    return -1
+
+
+tests = []
+
+test = {
+    'input': {
+        'array': [1, 3, 3, 3, 7, 10, 15],
+        'target': 1
+    },
+    'output': [0, 0]
+}
+
+tests.append({
+    'input'
+})
