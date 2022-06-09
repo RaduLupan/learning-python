fc9cceaae28728ed91709a3222a439c062a417f8
RaduLupan <github@lupan.ca>
2022-06-09 07:09:56 -0400

    Created binary_search function.

--- python-dsa/problem-1.py
+++ python-dsa/problem-1.py
@@ -53,7 +53,18 @@ def binary_search(lo,hi,condition):
     Description: Generic algorithm for binary search. The condition argument is a function that determines whether the answer lies before, after or at a given position.
     Use in conjunction with locate_card function.
     '''
-    pass
+    while lo <= hi:
+        mid = (lo+hi) // 2
+        
+        result = condition(mid)
+
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid-1
+        elif result == 'right':
+            lo = mid+1
+    return -1
 
 def locate_card(cards, query):
     '''
