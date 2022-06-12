2536c0d6f3a26aedc9957bcae0d549082ff72dea
RaduLupan <github@lupan.ca>
2022-06-12 08:01:45 -0400

    Added summary of passed and failed tests to evaluate_test_cases function.

--- python-dsa/dsa.py
+++ python-dsa/dsa.py
@@ -28,7 +28,6 @@ def evaluate_test_case(function, test):
     - function: the name of the function whose output is evaluated against test output.
     - test: test case in the form of a dictionary where test['input'] must match the arguments of function and test['output'] containes the tested output.
     TODO: Add measure of execution time for the function tested.
-    TODO: Add summary of tests passed and failed.
     '''    
     print(f"Input: {test['input']}")
 
@@ -43,6 +42,7 @@ def evaluate_test_case(function, test):
     else:
         print ("Test Result: FAILED\n")
 
+
 def evaluate_test_cases(function, tests):
     '''
     Description: Uses evaluate_test_case to compare actual output of a function with a series of test cases.
@@ -50,6 +50,25 @@ def evaluate_test_cases(function, tests):
     - function: the name of the function whose output is evaluated against test output.
     - tests: a list of test dictionaries where test['input'] must match the arguments of function and test['output'] containes the tested output.
     '''
+    pass_count = 0
+    fail_count = 0
+
     for test in tests:
         print(f"TEST#: {tests.index(test)}")
-        evaluate_test_case(function=function, test=test)        
+        
+        print(f"Input: {test['input']}")
+
+        print (f"Expected Output: {test['output']}")
+    
+        result = function(**test['input'])
+
+        print(f"Actual Output: {result}")   
+        
+        if result == test['output']:
+            print("Test Result: PASSED\n")
+            pass_count += 1
+        else:
+            print ("Test Result: FAILED\n")
+            fail_count += 1
+    
+    print(f"SUMMARY\n TOTAL: {len(tests)}, PASSED: {pass_count}, FAILED: {fail_count}")
