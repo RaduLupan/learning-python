ec34bb0b3542e2b45931eb086b49c9dfd8efe397
RaduLupan <github@lupan.ca>
2022-06-15 09:59:54 -0400

    Initial commit

---
+++ examples/fibonacci.py
@@ -0,0 +1,8 @@
+'''
+Problem: Write a recursive function that generates the first n elements of a Fibonacci sequence.
+fibo(0) = 0
+fibo(1) = 1
+fibo(n) = fibo(n-1) + fibo(n-2)
+'''
+def fibonacci(n):
+    pass
