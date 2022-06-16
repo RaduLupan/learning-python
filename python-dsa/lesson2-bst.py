61920e775403411c275a7dd50eea63d4b51c9b09
RaduLupan <github@lupan.ca>
2022-06-16 08:24:24 -0400

    If __str__() is not defined then the __repr__() is called instead

--- python-dsa/lesson2-bst.py
+++ python-dsa/lesson2-bst.py
@@ -6,13 +6,10 @@ class User:
     
     def __repr__(self):
         return f"User(username={self.username}, name={self.name}, email={self.email})"
-        
-    def __str__(self):
-        return f" The User with username = {self.username} has the following properties: name = {self.name} and email = {self.email}"
 
 
 user1=User('bobg', 'Bob Green', 'bobg@example.com')
 
-print(user1.__repr__())
-print(user1.__str__())
+print(user1)
+
 
