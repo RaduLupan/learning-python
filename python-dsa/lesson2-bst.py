231a47ac675294c9efa0f6649681c68f58f6c053
RaduLupan <github@lupan.ca>
2022-06-16 08:33:15 -0400

    Added problem statement and defined UserDatabase class.

--- python-dsa/lesson2-bst.py
+++ python-dsa/lesson2-bst.py
@@ -1,3 +1,7 @@
+'''
+Problem: We need to create a data structure which can store 100 million records and perform insertion, search, update and list operations efficiently.
+Input: User profiles which contain username, name and email for a user.
+'''
 class User:
     def __init__(self, username, name, email):
         self.username = username
@@ -7,6 +11,16 @@ class User:
     def __repr__(self):
         return f"User(username={self.username}, name={self.name}, email={self.email})"
 
+
+class UserDatabase:
+    def insert(self, user):
+        pass
+    def find(self, username):
+        pass
+    def update(self, user):
+        pass
+    def list_all(self):
+        pass
 
 user1=User('bobg', 'Bob Green', 'bobg@example.com')
 
