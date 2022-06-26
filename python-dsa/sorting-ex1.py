8cde0768a4f68d323e3b0b525284a0605a48fc61
RaduLupan <github@lupan.ca>
2022-06-26 09:07:16 -0400

    Implemented bubble_sort and tested ok

--- python-dsa/sorting-ex1.py
+++ python-dsa/sorting-ex1.py
@@ -6,8 +6,31 @@ def bubble_sort(array):
     '''
     Description: Sorts a list using the Bubble Sort algorithm.
     Algorithm:
-    1. Iterate through the items in the array multiple times, at each iteration compare the current item with the adjacent one.
-    2. If the current item is greater than the adjacent one swap their places so that the greater item moves towards its correct position in the array.
-    3. Repeat step 2 for until the second last item in the array.
-    4. Repeat steps 1-3 until there are no more swaps to be done which means the array is sorted in ascending order.
+    1. Make multiple passes through an array.
+    2. Compare elements one by one and swap adjacent elements that are out of order.
+    3. Repeat steps 1-2 until there are no more swaps to be done which means the array is sorted in ascending order.
     '''
+
+    n = len(array)
+
+    for i in range(n):
+        
+        already_sorted = True
+
+        for j in range(n-i-1):
+            
+            print(f"i = {i},  j = {j}, array[j] = {array[j]}, array[j+1] = {array[j+1]}, array = {array}")
+            if array[j] > array [j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] =  temp
+                already_sorted = False
+
+        if already_sorted:
+            break
+    
+    return array
+
+list=[1,2,3,4,5]
+sorted_list=bubble_sort(list)
+print(sorted_list)
