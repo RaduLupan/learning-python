e51f7487362764e9cebcd7da2b55dd92516714c1
RaduLupan <github@lupan.ca>
2022-07-04 08:50:02 -0400

    Added __repr__ and parse_tuple methods.

--- python-dsa/dsa.py
+++ python-dsa/dsa.py
@@ -101,3 +101,41 @@ class TreeNode():
         if self is None:
             return []
         return TreeNode.traverse_post_order(self.left) + TreeNode.traverse_post_order(self.right) + [self.key]
+
+    def display_keys(self, space='\t', level=0):
+        # If the node is empty
+        if self is None:
+            print(space*level + '∅')
+            return   
+
+        # If the node is a leaf 
+        if self.left is None and self.right is None:
+            print(space*level + str(self.key))
+            return
+
+        # If the node has children
+        display_keys(self.right, space, level+1)
+        print(space*level + str(self.key))
+        display_keys(self.left,space, level+1)   
+
+    def to_tuple(self):
+        if self is None:
+            return None
+        if self.left is None and self.right is None:
+            return self.key
+        return TreeNode.to_tuple(self.left), self.key, TreeNode.to_tuple(self.right)
+
+    def __repr__(self):
+        return "BinaryTree <{}>".format(self.to_tuple())
+    
+    @staticmethod
+    def parse_tuple(data):
+        if data is None:
+            node is None
+        elif isinstance(data, tuple) and len(data) == 3:
+            node = TreeNode(data[1])
+            node.left = TreeNode.parse_tuple(data[0])
+            node.right = TreeNode.parse_tuple(data[2])
+        else:
+            node = TreeNode(data)
+        return node
