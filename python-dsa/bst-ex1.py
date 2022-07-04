42eff900f9831853b1f2371737a1fea0827211b0
RaduLupan <github@lupan.ca>
2022-07-04 10:37:39 -0400

    Added is_bst function and example call.

--- python-dsa/bst-ex1.py
+++ python-dsa/bst-ex1.py
@@ -10,10 +10,34 @@ Question 2: Write a function that finds the maximum key in a binary tree.
 Question 3: Write a function that finds the minimum key in a binary tree.
 '''
 def remove_none(nums):
+    '''
+    Description: Removes elements that are None in a list or tuple.
+    '''
     return [x for x in nums if x is not None]
 
+def is_bst(node):
+    '''
+    Description: Checks whether a binary tree is a BST or not while also returning the minimum and maximum keys in the binary tree.
+    '''
+
+    if node is None:
+        return True, None, None
+    
+    is_bst_left, min_left, max_left = is_bst(node.left)
+    is_bst_right, min_right, max_right = is_bst(node.right)
+
+    is_bst_node = is_bst_left and is_bst_right and (max_left is None or max_left < node.key) and (min_right is None or min_right > node.key)
+    
+    min_key = min(remove_none([min_left, node.key, min_right]))
+    max_key = max(remove_none([max_left, node.key, max_right]))
+
+    # print(node.key, min_key, max_key, is_bst_node)
+
+    return is_bst_node, min_key, max_key 
 import dsa
 
 tree = dsa.TreeNode.parse_tuple((((None, 25, (42, 36, 48)), 10, None), 7, ((None, 23, 35), 18, ((43, 39, None), 29, None))))
 print(tree)
 
+is_bst_tree = is_bst(tree)
+print(is_bst_tree)
