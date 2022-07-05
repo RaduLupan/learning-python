cfdc032229d4573587b0f509b0a7834e36dc5c88
RaduLupan <github@lupan.ca>
2022-07-05 08:21:40 -0400

    Added user examples, created non-BST and BST tree examples.

--- python-dsa/bst-ex1.py
+++ python-dsa/bst-ex1.py
@@ -36,8 +36,57 @@ def is_bst(node):
     return is_bst_node, min_key, max_key 
 import dsa
 
-tree = dsa.TreeNode.parse_tuple((((None, 25, (42, 36, 48)), 10, None), 7, ((None, 23, 35), 18, ((43, 39, None), 29, None))))
-print(tree)
+tree1 = dsa.TreeNode.parse_tuple((((None, 25, (42, 36, 48)), 10, None), 7, ((None, 23, 35), 18, ((43, 39, None), 29, None))))
+print(tree1)
 
-is_bst_tree = is_bst(tree)
+is_bst_tree = is_bst(tree1)
 print(is_bst_tree)
+
+bobg=dsa.User('bobg', 'Bob Green', 'bobg@example.com')
+aliceb=dsa.User('aliceb', 'Alice Brown', 'aliceb@example.com')
+joeb=dsa.User('joeb', 'Joe Blue', 'joeb@example.com')
+danb=dsa.User('danb', 'Dan Black', 'danb@example.com')
+pamy=dsa.User('pamy', 'Pam Yellow', 'pamy@example.com')
+georgeo=dsa.User('georgeo', 'George Orange', 'georgeo@example.com')
+victorp=dsa.User('victorp', 'Victor Pink', 'victorp@example.com')
+
+tree2 = dsa.BSTNode(bobg.username, bobg)
+print(tree2.key, tree2.value)
+
+# Create tree2 example - Level 1
+tree2.left = dsa.BSTNode(aliceb.username, aliceb)
+tree2.right = dsa.BSTNode(pamy.username, pamy)
+
+# Create tree2 example - Level 2 -> Left
+tree2.left.left = dsa.BSTNode(joeb.username, joeb)
+tree2.left.right = dsa.BSTNode(danb.username, danb)
+
+# Create tree2 example - Level 2 -> Right
+tree2.right.left = dsa.BSTNode(georgeo.username, georgeo)
+tree2.right.right = dsa.BSTNode(victorp.username, victorp)
+
+# Check if tree2 is a BST
+is_bst_tree2 = is_bst(tree2)
+print(is_bst_tree2)
+
+# Add user elenab to replace aliceb and create a BST
+elenab=dsa.User('elenab', 'Elena Brown', 'elenab@example.com')
+
+# Create BST example tree3 - Level 0
+tree3 = dsa.BSTNode(georgeo.username, georgeo)
+
+# Create BST example tree3 - Level 1
+tree3.left = dsa.BSTNode(danb.username, danb)
+tree3.right = dsa.BSTNode(pamy.username, pamy)
+
+# Create BST example tree3 - Level 2 -> Left
+tree3.left.left = dsa.BSTNode(bobg.username, bobg)
+tree3.left.right = dsa.BSTNode(elenab.username, elenab)
+
+# Create BST example tree3 - Level 2 -> Right
+tree3.right.left = dsa.BSTNode(joeb.username, joeb)
+tree3.right.right = dsa.BSTNode(victorp.username, victorp)
+
+# Check if tree3 is a BST
+is_bst_tree3 = is_bst(tree3)
+print(is_bst_tree3)
