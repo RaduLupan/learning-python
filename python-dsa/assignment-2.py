2c21ce52762bfe1fd48a7e8779713b999cf245d3
RaduLupan <github@lupan.ca>
2022-07-11 08:27:14 -0400

    Rewrote the get_index function

--- python-dsa/assignment-2.py
+++ python-dsa/assignment-2.py
@@ -1,13 +1,27 @@
-def get_index(key):
+from dataclasses import dataclass
+
+
+def get_index(data_list, a_string):
     '''
-    Description: Simple hashing function that calculates the index of a key by adding up all Unicode numbers for all characters.
+    Description: Simple hashing function that uses the ord() function to convert all characters in a string into numbers.
+    Algorithm:
+    1. Iterate over the string, character-by-character.
+    2. Convert each character to a number using Python's built-in ord function.
+    3. Add the numbers for each character to obtain the hash for the entire string.
+    4. Take the reminder of the result with the size of the data list.
     '''
-    index = ord(key[0])
+    # Variable to store the result (updated for each iteration).
+    result = 0
 
-    for i in range(1,len(key)-1):
-        index += ord(key[i])
+    for a_character in a_string:
+        # Convert the character to a number using ord().
+        a_number = ord(a_character)
+        # Add the number to the result.
+        result += a_number
 
-    return index
+    # Get the remainder of the result with the size of the data list.
+    list_index = result % len(data_list)
 
-print(get_index('Dan'))
-print(f"get_index('a'): {get_index('a')}")
+    return list_index
+
+
