271f902e99d920306605b2b3430c436e5af8fb20
RaduLupan <github@lupan.ca>
2022-07-11 07:57:03 -0400

    Fixed logic for 1-character keys.

--- python-dsa/assignment-2.py
+++ python-dsa/assignment-2.py
@@ -2,9 +2,12 @@ def get_index(key):
     '''
     Description: Simple hashing function that calculates the index of a key by adding up all Unicode numbers for all characters.
     '''
-    index = 0
+    index = ord(key[0])
 
-    for i in range(len(key)-1):
+    for i in range(1,len(key)-1):
         index += ord(key[i])
 
     return index
+
+print(get_index('Dan'))
+print(f"get_index('a'): {get_index('a')}")
