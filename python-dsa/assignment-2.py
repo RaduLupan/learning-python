fcbf2f71b40bb566480fd0d66023e9ac5453cbd5
RaduLupan <github@lupan.ca>
2022-07-14 08:55:32 -0400

    Added get_valid_index and getitem methods.

--- python-dsa/assignment-2.py
+++ python-dsa/assignment-2.py
@@ -61,15 +61,46 @@ class BasicHashTable:
 
     def get_valid_index(self, key):
         '''
-        Uses Python hash() function to calculate the hash and linear probing to handle colisions.
+        Uses Python ord() function to calculate the hash and linear probing to handle colisions.
         '''
-        pass
+        
+        result = 0
+
+        for char in key:
+            result += ord(char)
+
+        idx = result % len(self.data_list) 
+
+        while True:
+            
+            kv = self.data_list[idx]
+
+            # If the element at the idx position is None then return idx as the valid index.
+            if kv is None:
+                return idx
+            
+            # If the key at the idx position matches the given key return idx as the valid index.
+            if kv[0] == key:
+                return idx
+
+            # Check the next position in the list.
+            idx += 1
+
+            # If the end of the list is reached go to the beginning of the list.
+            if idx == len(self.data_list):
+                idx = 0
+
     
     def __getitem__(self, key):
         '''
         Returns the value associated with a given key.
         '''
-        pass
+        # Calculate a valid index for the given key.
+        idx = self.get_valid_index(self.data_list, key)
+
+        kv = self.data_list[idx]
+
+        return kv[1] if kv is not None else None 
     
     def __setitem__(self, key, value):
         '''
@@ -78,14 +109,23 @@ class BasicHashTable:
         pass
 
 
-data_list = [None] * MAX_HASH_TABLE_SIZE
+#data_list = [None] * MAX_HASH_TABLE_SIZE
 
-print(len(data_list) == 4096)
+#print(len(data_list) == 4096)
 
-print(get_index(data_list, 'abc') == get_index(data_list, 'cba'))
-print(get_index(data_list, 'listen') == get_index(data_list, 'silent'))
+#print(get_index(data_list, 'abc') == get_index(data_list, 'cba'))
+#print(get_index(data_list, 'listen') == get_index(data_list, 'silent'))
 
-print(f"get_index2(data_list, 'abc'={get_index2(data_list, 'abc')}")
-print(f"get_index2(data_list, 'cba'={get_index2(data_list, 'cba')}")
+#print(f"get_index2(data_list, 'abc'={get_index2(data_list, 'abc')}")
+#print(f"get_index2(data_list, 'cba'={get_index2(data_list, 'cba')}")
 
-print(get_index2(data_list, 'listen') == get_index2(data_list, 'silent'))
+#print(get_index2(data_list, 'listen') == get_index2(data_list, 'silent'))
+
+basic_hash_table = BasicHashTable(max_size=10)
+
+print(basic_hash_table.data_list)
+
+print(basic_hash_table.get_valid_index('listen'))
+print(basic_hash_table.get_valid_index('silent'))
+
+#print(basic_hash_table.__getitem__('Dan'))
