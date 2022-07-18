ed7b55b83f85fa5c2cfd31b2afe0a2e15966faf6
RaduLupan <github@lupan.ca>
2022-07-18 06:49:24 -0400

    Initial commit

---
+++ python-dsa/lesson3-sorting.py
@@ -0,0 +1,11 @@
+'''
+Merge Sort, Quick Sort and Divide-n-Conquer Algorithms in Python
+
+Question 1: You're working on a new feature on Jovian called "Top Notebooks of the Week". 
+Write a function to sort a list of notebooks in decreasing order of likes. Keep in mind that up to millions
+of notebooks can be created every week, so your function needs to be as efficient as possible.
+
+Question 2: Write a program to sort a list of numbers.
+'''
+def sort(nums):
+    pass
