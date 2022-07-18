d3f12ddb8aad63ce09083cdd8c2263079cfe07ce
RaduLupan <github@lupan.ca>
2022-07-18 07:27:54 -0400

    Created test cases.

--- python-dsa/lesson3-sorting.py
+++ python-dsa/lesson3-sorting.py
@@ -9,3 +9,83 @@ Question 2: Write a program to sort a list of numbers.
 '''
 def sort(nums):
     pass
+
+# A list of numbers in random order.
+test0 = {
+    'input': {
+        'nums': [42, 10, 89, 76, 35, 68, 12, 90, 55]
+    },
+    'output': [10, 12, 35, 42, 55, 68, 76, 89, 90]
+}
+
+# A list of numbers in random order.
+test1 = {
+    'input': {
+        'nums': [133, -540, 420, 0, 167, 358, 205, -200]
+    },
+    'output': [-540, -200, 0, 133, 167, 205, 358, 420]
+}
+
+# A list of numbers that's already sorted.
+test2 = {
+    'input': {
+        'nums': [10, 20, 20, 40, 50, 60, 70, 80, 90]
+    },
+    'output': [10, 20, 20, 40, 50, 60, 70, 80, 90 ]
+}
+
+# A list that's sorted in descending order.
+test3 = {
+    'input': {
+        'nums': [400, 325, 280, 145, 98, 75, 64, 50]
+    },
+    'output': [50, 64, 75, 98, 145, 280, 325, 400]
+}
+
+# A list containing repeating elements.
+test4 = {
+    'input': {
+        'nums': [4, 2, 6, 3, 4, 6, 2, 1]
+    },
+    'output': [1, 2, 2, 3, 4, 4, 6, 6]
+}
+
+# An empty list.
+test5 = {
+    'input': {
+        'nums': []
+    },
+    'output': []
+}
+
+# A list containing just one element.
+test6 = {
+    'input': {
+        'nums': [1000]
+    },
+    'output': [1000]
+}
+
+# A list containing one element repeated many times.
+test7 = {
+    'input': {
+        'nums': [500, 500, 500, 500, 500, 500, 500]
+    },
+    'output': [500, 500, 500, 500, 500, 500, 500]
+}
+
+# A really long list 10000 elements.
+import random
+in_list = list(range(10000))
+out_list = list(range(10000))
+
+random.shuffle(in_list)
+
+test8 = {
+    'input': {
+        'nums': in_list
+    },
+    'output': out_list
+}
+
+tests = [test0, test1, test2, test3, test4, test5, test6, test7, test8]
