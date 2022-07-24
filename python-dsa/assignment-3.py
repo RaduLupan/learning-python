37e86be92b431734c89c54077d69d2bb10477dc1
RaduLupan <github@lupan.ca>
2022-07-24 10:13:15 -0400

    Initial commit

---
+++ python-dsa/assignment-3.py
@@ -0,0 +1,10 @@
+'''
+Divide-n-Conquer Algorithms in Python
+
+Problem 1:
+Given two polynomials represented by two lists, write a function that efficiently multiplies given two polynomials.
+Example: 
+The lists [2, 0, 5, 7] and [3, 4, 2] represent the polynomials 2 + 5*x**2 + 7*x**3 and 3 + 4*x + 2*x**2.
+Their product is: 6 + 8*x + 19*x**2 + 41*x**3 + 38*x**4 + 14*x**5 
+which is represented by the list [6, 8, 19, 41, 38, 14]
+'''
