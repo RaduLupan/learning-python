16db51356570088f581b1ae9864372e788e16198
RaduLupan <github@lupan.ca>
2022-09-08 08:46:44 -0400

    Added function signature and docs.

--- python-dsa/lesson4-rdp.py
+++ python-dsa/lesson4-rdp.py
@@ -4,6 +4,17 @@ Question 1: Write a function to find the length of the longest common subsequenc
 Example: Given the strings "serendipitous" and "precipitation", the longest common subsequence is "reipito" and the length is 7.
 '''
 
+def lcs_recursive(seq1, seq2, idx1=0, idx2=0):
+    '''
+    Description: Computes the longest common subsequence of two sequences recursively.
+    Algorithm:
+    1. Define two counters idx1 for seq1 and idx2 for seq2 that are both zero initially. Compute the LCS of seq1[idx1:] and seq2[idx2:].
+    2. If seq1[idx1] is equal with seq2(idx2) then this character belongs to the LCS of seq1[idx1:] and seq2[idx2:]. Further the LCS of seq1[idx1:]
+    and seq2[idx2:] is 1 + the LCS of seq1[idx1+1:] and seq2[idx2+1:].
+    3. If not, compute the LCS of seq1[idx1+1:] and seq2[idx2:] and the LCS of seq1[idx1:] and seq2[idx2+1:] the maximum of which is the solution.
+    4. If either seq1[idx1:] or seq2[idx2:] is empty then their LCS is also empty.
+    '''
+    pass
 # Test cases.
 
 # 1. General case - string.
