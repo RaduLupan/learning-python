1cbc74d76641669d0a43c1f05d22a3d803bc50cd
RaduLupan <github@lupan.ca>
2022-09-11 09:06:18 -0400

    Added function signature and test cases.

--- python-dsa/lesson4-rdp-q2.py
+++ python-dsa/lesson4-rdp-q2.py
@@ -7,3 +7,60 @@ General problem statement:
     Given n elements, each of which has a weight and a profit, determine the maximum profit that can be obtained by selecting a subset of the elements weighing 
     no more than w.
 '''
+
+def max_profit(profits, weights, capacity):
+    pass
+
+# Test cases.
+
+# General case 1.
+test0 = {
+    'input': {
+        'profits': [5, 3, 7, 2, 10],
+        'weights': [7, 1, 10, 4, 11],
+        'capacity': 15
+    },
+    'output': 13
+}
+
+# General case 2.
+test1 = {
+    'input': {
+        'profits': [15, 30, 45, 12, 17, 25],
+        'weights': [13, 27, 41, 10, 11, 19],
+        'capacity': 80
+    },
+    'output': 92
+}
+
+# All weights are larger than the capacity.
+test2 = {
+    'input': {
+        'profits': [10, 20, 30, 40, 50, 60],
+        'weights': [15, 25, 35, 45, 55, 65],
+        'capacity': 10
+    },
+    'output': 0
+}
+
+# All weights fit into the capacity.
+test3 = {
+    'input': {
+        'profits': [17, 302, 250, 180, 78, 13],
+        'weights': [12, 185, 110, 90, 45, 9],
+        'capacity': 500
+    },
+    'output': 840
+}
+
+# Only one element is selected.
+test4 = {
+    'input': {
+        'profits': [7, 12, 5, 4, 2],
+        'weights': [14, 25, 18, 7, 5],
+        'capacity': 5
+    },
+    'output': 2
+}
+
+tests = [test0, test1, test2, test3, test4]
