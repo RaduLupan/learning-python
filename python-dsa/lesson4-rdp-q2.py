03796f4ba1d9be97ca6d575562d210c3dff67caf
RaduLupan <github@lupan.ca>
2022-09-11 07:54:31 -0400

    Initial commit

---
+++ python-dsa/lesson4-rdp-q2.py
@@ -0,0 +1,9 @@
+'''
+Problem statement
+    You are in charge of selecting a football (soccer) team from a large pool of players. Each player has a cost, and a rating. 
+    You have a limited budget. What is the highest total rating of a team that fits within your budget. Assume that there is no minimum or maximum team size.
+
+General problem statement:
+    Given n elements, each of which has a weight and a profit, determine the maximum profit that can be obtained by selecting a subset of the elements weighing 
+    no more than w.
+'''
