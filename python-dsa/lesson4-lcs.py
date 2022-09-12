56a53fc4d9b027a6e269a14433daf3e71a6f1bbd
RaduLupan <github@lupan.ca>
2022-09-12 08:10:33 -0400

    Added function signature and test case structure.

--- python-dsa/lesson4-lcs.py
+++ python-dsa/lesson4-lcs.py
@@ -7,3 +7,20 @@ seq2 = 'precipitation'
 
 lcs = 'reipito'
 '''
+
+def lcs(seq1, seq2):
+    pass
+
+
+# Test cases.
+# General case 1.
+test0 = {
+    'input': {
+        'seq1': 'serendiptous',
+        'seq2': 'precipitation'
+    },
+    'output': 'reipito'
+}
+
+# Evaluate first test case.
+assert lcs(**test0['input']) == test0['output']
