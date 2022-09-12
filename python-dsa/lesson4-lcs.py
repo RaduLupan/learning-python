1b952a1974e738fd00eadf7994c3e49d57dc8a17
RaduLupan <github@lupan.ca>
2022-09-12 08:02:12 -0400

    Initial commit

---
+++ python-dsa/lesson4-lcs.py
@@ -0,0 +1,9 @@
+'''
+Question: Write a modified version of the lcs() function that returns the longest common subsequence (LCS) of two sequences rather than the length of it.
+
+Example:
+seq1 = 'serendiptous'
+seq2 = 'precipitation'
+
+lcs = 'reipito'
+'''
