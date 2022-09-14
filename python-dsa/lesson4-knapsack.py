bdaf0b73cac5ef8195eec4ef6423f560903fcd87
RaduLupan <github@lupan.ca>
2022-09-14 07:49:10 -0400

    Initial commit

---
+++ python-dsa/lesson4-knapsack.py
@@ -0,0 +1,9 @@
+'''
+Modified Solution for the Knapsack Problem - Return the list of elements whose profits make up the maximum profit.
+
+Originally the Knapsack problem statement was:
+Given n elements, each of which has a weight and a profit, determine the maximum profit that can be obtained by selecting a subset of the elements weighing 
+no more than w.
+
+Write a modified version of the max_profit function that returns the list of elements selected to maximize profit. 
+'''
