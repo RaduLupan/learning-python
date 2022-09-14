dda5cb6b84a97e278bc7e4bd6aab6f5eee19c17b
RaduLupan <github@lupan.ca>
2022-09-14 09:06:38 -0400

    Implemented function and evaluated first test case ok.

--- python-dsa/lesson4-knapsack.py
+++ python-dsa/lesson4-knapsack.py
@@ -16,7 +16,7 @@ Inputs:
     Example: 15.
 
 Outputs:
-    index - a list of indices in the profits/weights lists for elements that if selected obtain the maximum profit for the given capacity.
+    indices - a list of positions in the profits/weights lists for elements that if selected obtain the maximum profit for the given capacity.
     For the example inputs above, the output is [1,4] which means that the maximum profit is profits[1] + profits[4] = 3 + 10 = 13 and
     the used capacity is weights[1] + weights[4] = 1 + 11 = 12 < capacity.
 
@@ -25,7 +25,37 @@ def max_profits_recursive(profits, weights, capacity, idx=0):
     '''
     Description: Returns the list of indices for elements in profits list that maximize the profit while staying within given weight (capacity).
     '''
-    pass
+    
+    indices = []
+
+    # Check if the profits/weights lists are exhausted.
+    if idx == len(profits):
+        return []
+    
+    # Check if the current element's weight is bigger than the capacity.
+    elif weights[idx] > capacity:
+        return max_profits_recursive(profits, weights, capacity, idx+1)
+    
+    # If the current element fits into the capacity, compute the two subproblems: either pick the element or don't and return the maximum. 
+    else:
+        # We pick the current element.
+        indices.append(idx)
+        option1 =  indices + max_profits_recursive(profits, weights, capacity-weights[idx], idx+1)
+        option2 = max_profits_recursive(profits, weights, capacity, idx+1)
+
+        # Compute the profit for the two options (list of indices) and return the list with greater profit as the solution.
+        profit1, profit2 = 0, 0
+        
+        for i in option1:
+            profit1 += profits[i]
+        for i in option2:
+            profit2 += profits[i]
+        
+        if profit1 > profit2:
+            return option1
+        else:
+            return option2
+
 # Test cases.
 
 # General case 1.
@@ -37,3 +67,7 @@ test0 = {
     },
     'output': [1, 4]
 }
+
+# Evaluate one test case.
+import dsa
+dsa.evaluate_test_case(max_profits_recursive, test0)
