43be9c408a1d01ea628a3af8b72b196664ec59e5
RaduLupan <github@lupan.ca>
2022-09-17 09:49:38 -0400

    Defined graph class and initialized with graph example.

--- python-dsa/lesson5-graphs.py
+++ python-dsa/lesson5-graphs.py
@@ -3,4 +3,21 @@ Question: Create a class to represent a graph as an adjacency list.
 '''
 
 class Graph:
-    pass
+    def __init__(self, num_nodes, edges):
+        self.num_nodes = num_nodes
+        
+        # List of empty lists.
+        self.data = [[] for _ in range(num_nodes)] 
+
+        # Each edge shows in two lists corresponding to the two nodes it connects.
+        for n1, n2 in edges:
+            self.data[n1].append(n2)
+            self.data[n2].append(n1)
+ 
+
+num_nodes=5
+edges=[(0,1), (0,4), (1,2), (1,3), (1,4), (2,3), (3,4)]
+
+graph=Graph(num_nodes, edges)
+
+print(graph.data)
