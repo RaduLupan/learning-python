61584a7d90513e702f6a74060c5442b341dcf23c
RaduLupan <github@lupan.ca>
2022-09-19 09:43:14 -0400

    Added __repr__ method to the Graph class.

--- python-dsa/lesson5-graphs.py
+++ python-dsa/lesson5-graphs.py
@@ -14,6 +14,8 @@ class Graph:
             self.data[n1].append(n2)
             self.data[n2].append(n1)
  
+    def __repr__(self):
+       return "\n".join(["{}:{}".format(node, neighbors) for node, neighbors in enumerate(self.data)])
 
 num_nodes=5
 edges=[(0,1), (0,4), (1,2), (1,3), (1,4), (2,3), (3,4)]
@@ -21,3 +23,4 @@ edges=[(0,1), (0,4), (1,2), (1,3), (1,4), (2,3), (3,4)]
 graph=Graph(num_nodes, edges)
 
 print(graph.data)
+print(repr(graph))
