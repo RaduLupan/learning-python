b26f42e787fa18d0258e54fd54f95e81945b0caa
RaduLupan <github@lupan.ca>
2022-09-20 08:33:34 -0400

    Added add_edge method to the Graph class and tested ok.

--- python-dsa/lesson5-graphs.py
+++ python-dsa/lesson5-graphs.py
@@ -1,5 +1,7 @@
 '''
-Question: Create a class to represent a graph as an adjacency list.
+Question1: Create a class to represent a graph as an adjacency list.
+Question2: Write a function to add an edge to a graph represented as an adjancecy list.
+Question3: Write a function to remove an edge from a graph represented as an adjancecy list.
 '''
 
 class Graph:
@@ -14,6 +16,18 @@ class Graph:
             self.data[n1].append(n2)
             self.data[n2].append(n1)
  
+    def add_edge(self, n1, n2):
+        
+        # Brings the nodes within the Graph in case n1,n2 >= len(self.data)
+        n1 = n1 % len(self.data)
+        n2 = n2 % len(self.data)
+
+        # Avoids adding the same edge multiple times.
+        if n2 not in self.data[n1]:
+            self.data[n1].append(n2)
+        if n1 not in self.data[n2]:
+            self.data[n2].append(n1)
+
     def __repr__(self):
        return "\n".join(["{}:{}".format(node, neighbors) for node, neighbors in enumerate(self.data)])
 
@@ -22,5 +36,8 @@ edges=[(0,1), (0,4), (1,2), (1,3), (1,4), (2,3), (3,4)]
 
 graph=Graph(num_nodes, edges)
 
-print(graph.data)
-print(repr(graph))
+graph.add_edge(0,3)
+print(graph)
+
+graph.add_edge(0,3)
+print(graph)
