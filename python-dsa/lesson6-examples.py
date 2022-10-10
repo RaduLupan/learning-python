38a6910003cde01e8a60ef50a9a038e6dd87dd75
RaduLupan <github@lupan.ca>
2022-10-10 11:53:47 -0400

    Added a few test cases and function signature.

--- python-dsa/lesson6-examples.py
+++ python-dsa/lesson6-examples.py
@@ -17,9 +17,62 @@ Problem1: You are given an array of numbers (non-negative). Find a continuous su
 # 1. State the problem clearly. Identify the input & output formats.
 
 # Input: arr - a list of positive numbers, target - a number representing the target sum.
-arr1 = [1, 4, 7, 5, 10, 13]
-target1 = 16
+# Example:
+# arr = [1, 4, 7, 5, 10, 13]
+# target = 16
 
-# Output: (i, j) - a tuple representing the start and end indices for the sub array in arr whose elements add up to target.
-output1 = 1, 3 
+# Output: (i, j) - a tuple representing the start and end indices for the subarray in arr whose elements add up to target.
+# output1 = 1, 3 
 
+# 2. Come up with some examples of inputs and outputs. Try to cover all edge cases.
+# 2.1 General case: the subarray is in the middle of the arr list.
+test0 = {
+    'input': {
+        'arr': [1, 4, 7, 5, 10, 13],
+        'target': 16
+    },
+    'output': (1,3)
+}
+
+# 2.2 The subarray is at the end of arr list.
+test1 = {
+    'input': {
+        'arr': [5, 10, 3, 15, 9, 20, 7],
+        'target': 51
+    },
+    'output': (3, 6)
+}
+
+# 2.3 The subarray is at the beginning of arr list.
+test2 = {
+    'input': {
+        'arr': [5, 10, 3, 15, 9, 20, 7],
+        'target': 18
+    },
+    'output': (0, 2)
+}
+
+# 2.4 The subarray has only 1 element.
+test3 = {
+    'input': {
+        'arr': [5, 10, 3, 15, 9, 20, 7],
+        'target': 10
+    },
+    'output': (1, 1)
+}
+
+# The subarray has zero elements.
+test4 = {
+    'input': {
+        'arr': [5, 10, 3, 15, 9, 20, 7],
+        'target': 1000
+    },
+    'output': (None, None)
+}
+
+tests = [test0, test1, test2, test3, test4]
+
+def find_subarray1(arr, target):
+    pass
+
+print(find_subarray1(**test0['input']))
