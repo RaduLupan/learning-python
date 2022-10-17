3a23248ffc7b63d2fcfb9ff8ed413019a5096c55
RaduLupan <github@lupan.ca>
2022-10-17 17:20:03 -0400

    Renamed exemple1 file and created example2.

---
+++ python-dsa/lesson6-example2.py
@@ -0,0 +1,17 @@
+'''
+The following systematic strategy for solving problems will be used for the two problems below.
+
+The method
+1. State the problem clearly. Identify the input & output formats.
+2. Come up with some examples of inputs and outputs. Try to cover all edge cases.
+3. Come up with a crrect solution for the problem. State it in plain English.
+4. Implement the solution and test it using the example inputs. Fix bugs if any.
+5. Analyze the algorithm's complexity and identify inefficiencies if any.
+6. Apply the right technique to overcome the inefficiencies. Repeat steps 3-6. 
+
+Problem2: Given two strings A and B, find the minimum number of steps required to convert A to B. You have the following 3 operations permitted on a word:
+-> Insert a character
+-> Delete a character
+-> Replace a character
+Eeach operation is counted as 1 step.
+'''
