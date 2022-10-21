eb3d79a05354a4c527a51def42bdba9b3af523ef
RaduLupan <github@lupan.ca>
2022-10-21 08:47:11 -0400

    Initial commit.

---
+++ python-dsa/project.py
@@ -0,0 +1,39 @@
+'''
+The following systematic strategy for solving problems will be used for the problem below.
+
+The method
+1. State the problem clearly. Identify the input & output formats.
+2. Come up with some examples of inputs and outputs. Try to cover all edge cases.
+3. Come up with a correct solution for the problem. State it in plain English.
+4. Implement the solution and test it using the example inputs. Fix bugs if any.
+5. Analyze the algorithm's complexity and identify inefficiencies if any.
+6. Apply the right technique to overcome the inefficiencies. Repeat steps 3-6. 
+
+Problem: Reverse a Linked List in Groups of Given Size
+Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. 
+If the number of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed
+(See Example 2 for clarification).
+
+Example 1:
+Input:
+LinkedList: 1->2->2->4->5->6->7->8
+K = 4
+Output: 4 2 2 1 8 7 6 5 
+Explanation: 
+The first 4 elements 1,2,2,4 are reversed first 
+and then the next 4 elements 5,6,7,8. Hence, the 
+resultant linked list is 4->2->2->1->8->7->6->5.
+
+Example 2:
+Input:
+LinkedList: 1->2->3->4->5
+K = 3
+Output: 3 2 1 5 4 
+Explanation: 
+The first 3 elements are 1,2,3 are reversed 
+first and then elements 4,5 are reversed.Hence, 
+the resultant linked list is 3->2->1->5->4.
+
+Reference:
+https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1?page=3&company[]=Amazon&sortBy=submissions
+'''
