341927ac52424f6964a37d301d4538311fe39bff
RaduLupan <github@lupan.ca>
2022-11-01 08:31:08 -0400

    Added describe_security_group_rules

--- aws-sdk-examples/securitygroups-ex2.py
+++ aws-sdk-examples/securitygroups-ex2.py
@@ -2,3 +2,16 @@ import boto3
 
 # Get the service resource
 client = boto3.client('ec2')
+
+filters=[
+        {
+            'Name': 'group-id',
+            'Values': ['sg-09d0c55a2a08dcadb']
+        }
+]
+
+response = client.describe_security_group_rules(
+    Filters = filters
+)
+
+print(response)
