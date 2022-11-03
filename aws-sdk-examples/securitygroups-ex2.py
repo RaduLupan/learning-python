fcb02c4fdddf26ef7a80d00d02dba951585630c4
RaduLupan <github@lupan.ca>
2022-11-03 09:53:40 -0400

    Implemented detection of ingress rules that allow RDP or SSH access from anywhere.

--- aws-sdk-examples/securitygroups-ex2.py
+++ aws-sdk-examples/securitygroups-ex2.py
@@ -1,12 +1,14 @@
 import boto3
 
+sg_id = 'sg-09d0c55a2a08dcadb'
+
 # Get the service resource
 client = boto3.client('ec2')
 
 filters=[
         {
             'Name': 'group-id',
-            'Values': ['sg-09d0c55a2a08dcadb']
+            'Values': [sg_id]
         }
 ]
 
@@ -14,4 +16,23 @@ response = client.describe_security_group_rules(
     Filters = filters
 )
 
-print(response)
+offending_sg_rules = []
+
+sg_rules = response['SecurityGroupRules']
+
+for rule in sg_rules:
+    # Only flag ingress rules with CidrIpv4 equal to anywhere.
+    if (rule['IsEgress'] == False) and (rule['CidrIpv4'] == '0.0.0.0/0'):
+        
+        # Flag the rules that allow RDP or SSH access from anywhere.
+        if (rule['FromPort'] == 3389) or (rule['FromPort'] == 22):
+            print(f"Ingress rule {rule['SecurityGroupRuleId']} opens port {rule['FromPort']} from anywhere!" )
+            offending_sg_rules.append(rule)
+
+if len(offending_sg_rules) == 0:
+    print('No offending rules found.')        
+
+else:
+    print(f"Offending rules: {offending_sg_rules}")
+
+
