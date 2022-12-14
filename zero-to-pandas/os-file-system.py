597fc9b56924bb3299e9556bba6836097eca44df
RaduLupan <github@lupan.ca>
2022-12-14 09:47:37 -0500

    Added parse_values() function.

--- zero-to-pandas/os-file-system.py
+++ zero-to-pandas/os-file-system.py
@@ -3,3 +3,19 @@ def parse_headers(header_line):
     Returns a list of headers after stripping out the spaces, new line characters and commas.
     '''
     return header_line.strip().split(',')
+
+def parse_values(data_line):
+    '''
+    Returns a list of values from a comma separated data line.
+    '''
+    values = []
+
+    for item in data_line.strip().split(','):
+        if item == "":
+            values.append(0.0)
+        else:
+            try:
+                values.append(float(item))
+            except ValueError:
+                values.append(item)
+    return values
