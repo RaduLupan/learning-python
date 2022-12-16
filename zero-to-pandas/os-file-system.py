97345ccfd6238adce5b735299c30fcf27132dc8a
RaduLupan <github@lupan.ca>
2022-12-16 08:52:19 -0500

    Implemented read_csv() using helper function create_item_dict().

--- zero-to-pandas/os-file-system.py
+++ zero-to-pandas/os-file-system.py
@@ -19,3 +19,28 @@ def parse_values(data_line):
             except ValueError:
                 values.append(item)
     return values
+
+def read_csv(path):
+    '''
+    Reads lines from CSV file and returns a list of dictionaries representing each line.
+    '''
+
+    result = []
+
+    # Open the file in read mode.
+    with open(path, 'r') as f:
+        lines = f.readlines()
+
+    # Parse the headers.
+    headers = parse_headers(lines[0])
+
+    # Parse the remaining lines.
+    for data_line in lines[1:]:
+        # Parse the values.
+        values = parse_values(data_line)
+        # Create dictionary.
+        item_dict = create_item_dict(values, headers)
+        # Add dictionary to the result.
+        result.append(item_dict)
+    
+    return result
