9ac282fde6826825b9b158bead1b45bc735a7827
RaduLupan <github@lupan.ca>
2023-01-15 10:06:42 -0500

    Read the CSV file using Pandas.

--- zero-to-pandas/covid-data-analysis.py
+++ zero-to-pandas/covid-data-analysis.py
@@ -3,4 +3,13 @@ This notebook shows some examples of analyzing tabular data with Pandas.
 The source CSV is Our World in Data Covid data. 
 '''
 
+from urllib.request import urlretrieve
+import os
+
 owid_covid_url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
+
+# Create the data folder under current directory.
+os.makedirs('./data', exist_ok=True)
+
+# Download the covid data CSV file from OWID.
+urlretrieve(owid_covid_url, './data/owid-covid-data.csv')
