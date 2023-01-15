b73ce5c53a40dcef12bcec46c9fdfe0e8d59c4f4
RaduLupan <github@lupan.ca>
2023-01-15 09:34:31 -0500

    Initial commit.

---
+++ zero-to-pandas/covid-data-analysis.py
@@ -0,0 +1,6 @@
+'''
+This notebook shows some examples of analyzing tabular data with Pandas.
+The source CSV is Our World in Data Covid data. 
+'''
+
+owid_covid_url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
