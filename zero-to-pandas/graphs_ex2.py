484b1c67e1b201298f17c1353edf873bc96ddf1a
RaduLupan <github@lupan.ca>
2023-01-23 08:47:08 -0500

    Added scatterplot example

--- zero-to-pandas/graphs_ex2.py
+++ zero-to-pandas/graphs_ex2.py
@@ -1,2 +1,14 @@
+'''
+More graph examples using the Iris dataset included in Seaborn. 
+'''
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+flowers_df = sns.load_dataset('iris')
+
+plt.figure(figsize = (12,6))
+plt.title('Sepal Dimensions')
+
+sns.scatterplot(x = 'sepal_length', y = 'sepal_width', hue = 'species', s = 100, data = flowers_df)
+
+plt.show()
