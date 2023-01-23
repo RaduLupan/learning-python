c6a397860827f2d1a86f4487ba4ae2799a81bb23
RaduLupan <github@lupan.ca>
2023-01-22 19:27:25 -0500

    Set style using seaborn function, used fmt argument to set markers, line style and color.

--- zero-to-pandas/matplotlib_ex1.py
+++ zero-to-pandas/matplotlib_ex1.py
@@ -5,10 +5,16 @@ years = range(2000, 2012)
 apples = [0.895, 0.91, 0.919, 0.926, 0.929, 0.931, 0.934, 0.936, 0.937, 0.9375, 0.9372, 0.939]
 oranges = [0.962, 0.941, 0.930, 0.923, 0.918, 0.908, 0.907, 0.904, 0.901, 0.898, 0.9, 0.896, ]
 
-plt.plot(years, apples)
-plt.plot(years, oranges)
+# Set style globally.
+sns.set_style('darkgrid')
+
+# Plot the figure.
+plt.plot(years, apples, 's-b')
+plt.plot(years, oranges, 'o--r')
 plt.xlabel('Year')
-plt.ylabel('Yield (tons per hectare)');
+plt.ylabel('Yield (tons per hectare)')
+plt.title('Crop Yields in Kanto')
+plt.legend(['Apples','Oranges'])
 
 # Invoking .show() method is not required on a Juniper notebook but there you need an inline directive %matplotlib inline.
 plt.show()
