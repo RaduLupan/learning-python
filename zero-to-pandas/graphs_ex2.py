fe98bfa80b37db4edc5afe6aa285a03fca36a6af
RaduLupan <github@lupan.ca>
2023-01-24 09:08:44 -0500

    Added example for stacked histograms.

--- zero-to-pandas/graphs_ex2.py
+++ zero-to-pandas/graphs_ex2.py
@@ -21,9 +21,14 @@ virginica_df = flowers_df[flowers_df.species == 'virginica']
 
 
 # Draw multiple histograms on the same chart with reduced opacity so that the bars don't hide each other.
-plt.hist(setosa_df.sepal_width, alpha=0.4, bins=np.arange(2,5,0.25))
-plt.hist(versicolor_df.sepal_width, alpha=0.4, bins=np.arange(2,5,0.25))
-plt.hist(virginica_df.sepal_width, alpha=0.4, bins=np.arange(2,5,0.25))
+# plt.hist(setosa_df.sepal_width, alpha=0.4, bins=np.arange(2,5,0.25))
+# plt.hist(versicolor_df.sepal_width, alpha=0.4, bins=np.arange(2,5,0.25))
+# plt.hist(virginica_df.sepal_width, alpha=0.4, bins=np.arange(2,5,0.25))
+
+# We can also stack the histograms one on top of each other.
+plt.hist([setosa_df.sepal_width, versicolor_df.sepal_width, virginica_df.sepal_width],
+          bins=np.arange(2,5,0.25),
+          stacked=True)
 
 plt.title('Sepal Width Distribution')
 plt.legend(['Setosa', 'Versicolor', 'Virginica'])
