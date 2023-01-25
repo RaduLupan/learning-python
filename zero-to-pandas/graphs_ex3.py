f80eaed07c507921c47d12deef7dfdb6ed30b263
RaduLupan <github@lupan.ca>
2023-01-25 09:29:02 -0500

    Added barplot example.

--- zero-to-pandas/graphs_ex3.py
+++ zero-to-pandas/graphs_ex3.py
@@ -4,3 +4,14 @@ Bar Plots with Averages - Example
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Dataset example from Seaborn tips - information about the sex, time of day, total bill, and tip amount for customers visiting a restaurant over a week.
+tips_df = sns.load_dataset('tips')
+
+# Bar plots side by side based on hue.
+# sns.barplot(x = 'day', y = 'total_bill', hue = 'sex', data = tips_df)
+
+
+# You can make the bars horizontally simply by switching the axes.
+sns.barplot(x='total_bill', y='day', hue='sex', data=tips_df);
+plt.show()
