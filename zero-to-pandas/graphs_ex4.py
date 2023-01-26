2ca152f6718c270a197f438f8ec185619bee2944
RaduLupan <github@lupan.ca>
2023-01-26 09:01:43 -0500

    Improved heatmap by adding values in cells and changing color palette.

--- zero-to-pandas/graphs_ex4.py
+++ zero-to-pandas/graphs_ex4.py
@@ -8,8 +8,11 @@ import seaborn as sns
 flights_df = sns.load_dataset('flights').pivot('month', 'year', 'passengers')
 print(flights_df)
 
+
 plt.title('No. of Passangers (1000s)')
-sns.heatmap(flights_df)
 
+# sns.heatmap(flights_df)
 
+# We can also add the actual values in the cell blocks by specifying annot=True and using cmap argument to change the color palette. 
+sns.heatmap(flights_df, fmt='d', annot=True, cmap='Blues');
 plt.show()
