58116ad5ca880c5a60e6c86bec603309e81225f5
RaduLupan <github@lupan.ca>
2023-01-26 08:53:18 -0500

    Add heatmap example

--- zero-to-pandas/graphs_ex4.py
+++ zero-to-pandas/graphs_ex4.py
@@ -4,3 +4,12 @@ Heatmaps - Example
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+flights_df = sns.load_dataset('flights').pivot('month', 'year', 'passengers')
+print(flights_df)
+
+plt.title('No. of Passangers (1000s)')
+sns.heatmap(flights_df)
+
+
+plt.show()
