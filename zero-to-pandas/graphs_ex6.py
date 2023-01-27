68499ad371bd0b3116477b94cf175b0c74ed145c
RaduLupan <github@lupan.ca>
2023-01-27 08:57:21 -0500

    Added subplots

--- zero-to-pandas/graphs_ex6.py
+++ zero-to-pandas/graphs_ex6.py
@@ -4,3 +4,8 @@ Plotting Multiple Charts in a Grid - Example
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# Matplotlib and Seaborn support plotting multiple charts in a grid using plt.subplots which returns a set of axis for plotting.
+fig, axis = plt.subplots(2, 3, figsize = (16,8))
+
+plt.show()
