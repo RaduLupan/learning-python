853c693cffdee30cb12d90957d6fd7bf12634336
RaduLupan <github@lupan.ca>
2023-01-27 09:13:46 -0500

    Added the first chart on  axes[0,0].

--- zero-to-pandas/graphs_ex6.py
+++ zero-to-pandas/graphs_ex6.py
@@ -5,7 +5,19 @@ Plotting Multiple Charts in a Grid - Example
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Matplotlib and Seaborn support plotting multiple charts in a grid using plt.subplots which returns a set of axis for plotting.
-fig, axis = plt.subplots(2, 3, figsize = (16,8))
+years = range(2000, 2012)
+apples = [0.895, 0.91, 0.919, 0.926, 0.929, 0.931, 0.934, 0.936, 0.937, 0.9375, 0.9372, 0.939]
+oranges = [0.962, 0.941, 0.930, 0.923, 0.918, 0.908, 0.907, 0.904, 0.901, 0.898, 0.9, 0.896]
 
+
+# Matplotlib and Seaborn support plotting multiple charts in a grid using plt.subplots which returns a set of axis for plotting.
+fig, axes = plt.subplots(2, 3, figsize = (16,8))
+
+# Use the axis for plotting.
+axes[0,0].plot(years, apples, 's-b')
+axes[0,0].plot(years, oranges, 'o-r')
+axes[0,0].set_xlabel('Year')
+axes[0,0].set_ylabel('Yield (tons per hectare)')
+axes[0,0].legend(['Apples', 'Oranges'])
+axes[0,0].set_title('Crop Yields in Kanto')
 plt.show()
