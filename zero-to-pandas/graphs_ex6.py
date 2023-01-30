410c3ff2fa208106911b31f8625b8ce1cb4c44cf
RaduLupan <github@lupan.ca>
2023-01-30 08:11:58 -0500

    Added data for chart 3 and plotted histogram on chart 3.

--- zero-to-pandas/graphs_ex6.py
+++ zero-to-pandas/graphs_ex6.py
@@ -4,6 +4,7 @@ Plotting Multiple Charts in a Grid - Example
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 # Data for chart 1.
 years = range(2000, 2012)
@@ -12,6 +13,11 @@ oranges = [0.962, 0.941, 0.930, 0.923, 0.918, 0.908, 0.907, 0.904, 0.901, 0.898,
 
 # Data for chart 2.
 flowers_df = sns.load_dataset("iris")
+
+# Data for chart 3.
+setosa_df = flowers_df[flowers_df.species == 'setosa']
+versicolor_df = flowers_df[flowers_df.species == 'versicolor']
+virginica_df = flowers_df[flowers_df.species == 'virginica']
 
 # Matplotlib and Seaborn support plotting multiple charts in a grid using plt.subplots which returns a set of axis for plotting.
 fig, axes = plt.subplots(2, 3, figsize = (16,8))
@@ -32,4 +38,10 @@ sns.scatterplot(x = flowers_df.sepal_length,
                 s = 100,
                 ax = axes[0,1])
 
+# Use the axes for plotting.
+axes[0,2].set_title('Distribution of Sepal Width')
+axes[0,2].hist([setosa_df.sepal_width, versicolor_df.sepal_width, virginica_df.sepal_width],
+               bins=np.arange(2,5,0.25),
+               stacked=True)
+axes[0,2].legend(['setosa', 'versicolor', 'virginica'])
 plt.show()
